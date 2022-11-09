42b7a23a563aa7f5b0f057cc5847af69869c3654
yeongno <dldudsh83@gmail.com>
2022-11-10 02:41:45 +0900

    더미데이터 화면 마킹

--- src/config/tempClientConfig.js
+++ src/config/tempClientConfig.js
@@ -1,2 +1,2 @@
 //가까운 유저 찾기
-export const POST_NEARBYUSERS = "/dummy/IUserInfo.json"; //post api/uri/nearByUsers
+export const POST_NEARBYUSERS = "/dummy/IUserInfo.json"; //post api/uri/nearByUsers ({location : "lat , lng", radius : 1})
