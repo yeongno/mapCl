57c71c62f8af0a6f61d4c286ce6b337368842e53
lyn <dldudsh83@gmail.com>
2022-12-03 02:28:44 +0900

    아이디(이메일) 정규표현 사용. 중복체크는 기본 세팅만 하고 추후에 구현

--- src/components/views/header/RegisterPage/RegisterPage.jsx
+++ src/components/views/header/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,33 @@
 import { CheckOutlined, LockOutlined } from "@ant-design/icons";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../../styles/registerPage/RegisterPage.scss";
 
 function RegisterPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [checkEmail, setCheckEmail] = useState(false);
+  const isEmail = (email) => {
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    return emailRegex.test(email);
+  };
+  useEffect(() => {
+    setCheckEmail(isEmail(email));
+  }, [email]);
+
+  const onEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onCheckId = () => {
+    //todo
+    //중복 확인 api요청 후 true면 REF_idCheck를 이용하여 opacity 값 조정 후 disabled
+  };
+
   return (
     <div className="registerPage-container">
       <div className="registerPage-wrapper">
@@ -19,11 +42,13 @@ function RegisterPage() {
         </div>
         <div className="form-container">
           <span>아이디</span>
+          {!checkEmail && <span>제대로 입력해라</span>}
           <div className="form-id">
-            <button>
+            <button ref={REF_idCheck} onClick={onCheckId}>
               <CheckOutlined />
+              중복 확인
             </button>
-            <input name="id" />
+            <input name="id" onChange={onEmail} />
           </div>
           <span>비밀번호</span>
           <div className="form-password">
