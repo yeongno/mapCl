45746d4adb9ddde43d04493799c6609355546473
lyn <dldudsh83@gmail.com>
2022-12-02 19:35:55 +0900

    login form UI 세팅

--- src/components/views/header/LoginPage/LoginPage.jsx
+++ src/components/views/header/LoginPage/LoginPage.jsx
@@ -1,11 +1,15 @@
+import { CheckOutlined } from "@ant-design/icons";
+import { Checkbox } from "antd";
 import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../../redux/_actions/user_action";
+import "../../../styles/loginPage/LoginPage.scss";
 
 function LoginPage({ handleLogin }) {
   const [id, setId] = useState({});
   const [pw, setPW] = useState();
+  const [keepLogin, setKeepLogin] = useState(false);
   const disptach = useDispatch();
   const handleOnChangeId = (e) => {
     setId(e.target.value);
@@ -22,23 +26,6 @@ function LoginPage({ handleLogin }) {
       email: id,
       password: pw,
     };
-    // axios
-    //   .post("/user/login", data)
-    //   .then((response) => {
-    //     const { accessToken } = response.data.data.accessToken;
-    //     window.localStorage.setItem("token", response.data.data.accessToken);
-
-    //     // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-    //     axios.defaults.headers.common[
-    //       "jwt_access_token"
-    //     ] = `${response.data.data.accessToken}`;
-    //     console.log("accessToken", axios.defaults.headers);
-
-    //     // accessToken을 localStorage, cookie 등에 저장하지 않는다!
-    //   })
-    //   .catch((error) => {
-    //     // ... 에러 처리
-    //   });
     disptach(loginUser(data)).then((res) => {
       console.log(res);
     });
@@ -46,22 +33,52 @@ function LoginPage({ handleLogin }) {
 
   return (
     <div className="loginPage-container">
-      {/* 홈 로고 섹션 */}
-      <div className="topLogo-container"></div>
-      {/* 여러 형식의 입력 방식 */}
-      <div className="navForm-container"></div>
-      {/* 로그인 폼 */}
-      <div className="form-container">
-        <form>
-          <input type="text" name="id" onChange={handleOnChangeId} />
-          <input type="password" name="password" onChange={handleOnChangePW} />
-          <button type="submit" onClick={handleSubmit}>
-            LOGIN
-          </button>
-        </form>
+      <div className="loginPage-wrapper">
+        {/* 홈 로고 섹션 */}
+        <div className="topLogo-container"></div>
+        {/* 여러 형식의 입력 방식 */}
+        <div className="navForm-container"></div>
+        {/* 로그인 폼 */}
+        <div className="form-container">
+          <form className="form--common">
+            <input type="text" name="id" onChange={handleOnChangeId} />
+            <input
+              type="password"
+              name="password"
+              onChange={handleOnChangePW}
+            />
+            <div className="form-container__checkBox">
+              {(keepLogin && (
+                <CheckOutlined
+                  onClick={() => {
+                    setKeepLogin(false);
+                  }}
+                  style={{
+                    border: "solid 1px black",
+                  }}
+                />
+              )) || (
+                <CheckOutlined
+                  onClick={() => {
+                    setKeepLogin(true);
+                  }}
+                  style={{ color: "white", border: "solid 1px black" }}
+                />
+              )}{" "}
+              로그인 상태 유지
+            </div>
+            <div className="form__btn--submit" onClick={handleSubmit}>
+              <span>LOGIN</span>
+            </div>
+            <div className="form__btn--other" onClick={handleSubmit}>
+              <span>ID 찾기</span> <span>비밀번호 찾기</span>
+              <span>회원가입</span>
+            </div>
+          </form>
+        </div>
+        {/* 다른 소셜 계정 로그인 */}
+        <div className="navLogin-container"></div>
       </div>
-      {/* 다른 소셜 계정 로그인 */}
-      <div className="navLogin-container"></div>
     </div>
   );
 }
