29789ce5c04df94122d11ca3e238f935ba55708c
lyn <dldudsh83@gmail.com>
2023-02-02 01:16:05 +0900

    react-device-detect로 라이브러리 변경

--- src/components/views/LandingPage.jsx
+++ src/components/views/LandingPage.jsx
@@ -6,45 +6,13 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { GET_MYUSERINFO } from "../../config/tempClientConfig";
 import { tmpLogin } from "../../redux/_actions/user_action";
 import Header from "./header/Header";
-import Marker from "./map/Marker";
-import Marker2 from "./map/Marker2";
-import Marker3 from "./map/Marker3";
-import Marker4 from "./map/Marker4";
-import Marker5 from "./map/Marker5";
-import MarkerCluster from "./map/MarkerCluster";
-import Nomal from "./map/nomal";
-import Map1 from "./reactMap/Map1";
-import Map2 from "./reactMap/Map2";
-import Map3 from "./reactMap/Map3";
-import Map4 from "./reactMap/Map4";
-import Map5 from "./reactMap/Map5";
-import Map6 from "./reactMap/Map6";
-import Map7 from "./reactMap/Map7";
-import Sample from "./reactMap/Sample";
 import "../styles/LandingPage.scss";
-import MenuBar from "./manuBar/MenuBar";
-import { turnMenu } from "../../redux/_actions/turn_action";
 import useMenuSelector from "../../hook/navSelector/useMenuSelector";
-import Axios from "../../axios/Axios";
-import Test from "./test/Test";
-import PageLabel from "./common/pageLabel/PageLabel";
 import DrawerHeader from "./header/DrawerHeader";
-import DetailsComponent from "./test/Test copy 2";
-import ErrorBoundary from "antd/lib/alert/ErrorBoundary";
 import Loading from "./commnunity/loading/Loading";
-import { useMediaQuery } from "react-responsive";
+import { BrowserView, MobileView } from "react-device-detect";
 import Default from "./mobile/Default";
-
 function LandingPage({ handleLogin }) {
-  const isDesktopOrLaptop = useMediaQuery(
-    { minDeviceWidth: 1024 }
-    // { deviceWidth: 1600 } // `device` prop
-  );
-  const isMobileOrTablet = useMediaQuery(
-    { minDeviceWidth: 100 },
-    { maxDeviceWidth: 1023 }
-    // { deviceWidth: 1023 } // `device` prop
-  );
   const renderLoader = () => <Loading />;
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -60,22 +28,19 @@ function LandingPage({ handleLogin }) {
 
   return (
     <div className="LandingPage-container">
-      {isDesktopOrLaptop && (
-        <>
-          <DrawerHeader />
-          <Header />
-          <Suspense fallback={renderLoader()}>
-            <div className="basicBackground-container">
-              <Outlet />
-            </div>
-          </Suspense>
-        </>
-      )}
-      {isMobileOrTablet && (
-        <>
-          <Default />
-        </>
-      )}
+      <BrowserView>
+        <DrawerHeader />
+        <Header />
+        <Suspense fallback={renderLoader()}>
+          <div className="basicBackground-container">
+            <Outlet />
+          </div>
+        </Suspense>
+      </BrowserView>
+
+      <MobileView>
+        <Default />
+      </MobileView>
     </div>
   );
 }
--- src/components/views/mobile/Default.jsx
+++ src/components/views/mobile/Default.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Default() {
-  return <div>PC버전을 이용 바랍니다.</div>;
+  return <div style={{ background: "yellow" }}>PC버전을 이용 바랍니다.a</div>;
 }
 
 export default Default;
